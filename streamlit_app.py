e57d42c96dbce2530b89e83279fd2e769e8c315c
derek14 <wongwangfung@gmail.com>
2023-06-26 16:33:35 +0800

    Now it works

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-from utils import Cryptocurrencies, download_crypto_data
+from utils import Cryptocurrencies, download_crypto_data, download_valid_crypto_list, ordered_dict_to_dataframe, plot_covariance, show_efficient_froniter
 import pandas as pd
 import requests
 import time
@@ -15,37 +15,69 @@ from pypfopt.efficient_frontier import EfficientFrontier
 from Historic_Crypto import Cryptocurrencies, HistoricalData, LiveCryptoData
 import numpy as np
 from datetime import datetime
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-crypto_pairs = Cryptocurrencies().find_crypto_pairs()
-valid_crypto_pairs = crypto_pairs[(crypto_pairs["fx_stablecoin"]==False)&(crypto_pairs["status"]=="online")&(crypto_pairs["display_name"].str.endswith("/USD"))]
-
+valid_crypto_list = download_valid_crypto_list()
 st.title("Crypto Portfolio Optimizer")
 
 st.header("Inputs")
 selected_pairs = st.multiselect(
     'What crypto pairs do you want to include?',
-    valid_crypto_pairs["id"].unique().tolist(),
+    valid_crypto_list,
     ['BTC-USD', 'ETH-USD', 'ADA-USD', 'DOGE-USD', 'SOL-USD', 'LTC-USD'])
 
 start_date = st.date_input(
     "Start date of data analysis",
-    datetime.date(2022, 6, 25))
+    datetime(2022, 6, 25).date()).strftime('%Y-%m-%d-%H-%M')
 
 crypto_df = download_crypto_data(selected_pairs, start_date)
 
 st.header("Risk Modelling")
 mu = expected_returns.ema_historical_return(crypto_df, frequency=365)
 Sigma = risk_models.exp_cov(crypto_df, frequency=365)
+
 st.subheader("Correlation Plot")
-st.text("A correlation plot is a graphical representation of the correlation between two or more variables. It is a scatter plot with the values of one variable on the x-axis and the values of the other variable on the y-axis. Each point on the plot represents a pair of values for the two variables. The position of the point on the plot indicates the values of the two variables, and the color or size of the point indicates the strength of the correlation between the variables. The correlation between two variables is a measure of the strength and direction of the relationship between them. A positive correlation means that as one variable increases, the other variable also tends to increase. A negative correlation means that as one variable increases, the other variable tends to decrease. A correlation of zero means that there is no relationship between the variables.")
-# st.pyplot(plotting.plot_covariance(risk_models.exp_cov(crypto_df, frequency=365), plot_correlation=True))
+st.caption("A correlation plot is a graphical representation of the correlation between two or more variables. It is a scatter plot with the values of one variable on the x-axis and the values of the other variable on the y-axis. Each point on the plot represents a pair of values for the two variables. The position of the point on the plot indicates the values of the two variables, and the color or size of the point indicates the strength of the correlation between the variables.")
+st.caption("The correlation between two variables is a measure of the strength and direction of the relationship between them. A positive correlation means that as one variable increases, the other variable also tends to increase. A negative correlation means that as one variable increases, the other variable tends to decrease. A correlation of zero means that there is no relationship between the variables.")
+fig = plot_covariance(risk_models.exp_cov(crypto_df, frequency=365), plot_correlation=True)
+st.pyplot(fig)
+
+st.subheader("Covariance Plot")
+st.caption("Covariance is a statistical measure that describes the relationship between two random variables. It measures how much two variables vary together. If two variables have a positive covariance, it means that they tend to move together in the same direction. On the other hand, if two variables have a negative covariance, it means that they tend to move in opposite directions. The covariance value can range from -∞ to +∞, with a negative value indicating a negative relationship and a positive value indicating a positive relationship. The greater this number, the more reliant the relationship. Covariance is different from correlation, which measures the strength of the relationship between two variables. Covariance is an important tool in modern portfolio theory for determining what securities to put in a portfolio. A covariance matrix is a square matrix that illustrates the variance of dataset elements and the covariance between two datasets.")
+fig = plot_covariance(risk_models.exp_cov(crypto_df, frequency=365), plot_correlation=False)
+st.pyplot(fig)
+
+st.header("Get the Efficient Frontier")
+st.subheader("Efficient Frontier (Long Only)")
+st.caption("The efficient frontier is a set of ideal or optimal portfolios that offer the highest expected return for a specific level of risk. The standard deviation of returns in a portfolio measures investment risk and consistency in investment earnings. Lower covariance between portfolio securities results in lower portfolio standard deviation. Successful optimization of the return versus risk paradigm should place a portfolio along the efficient frontier line. Portfolios that lie below the efficient frontier are sub-optimal because they do not provide enough return for the level of risk.")
+st.caption("In order to diversify risks, it is important to choose uncorrelated assets. Keeping uncorrelated assets ensures that the entire portfolio is not killed by just one stray bullet. However, a portfolio consisting only of risky assets always has some risk, since most assets have some correlation because they are all subject to systemic risk.")
+st.caption("If a portfolio has very few assets that are highly correlated, it is not going to be great for several reasons. First, it will not be diversified enough, which means that it will not provide enough return for the level of risk. Second, the portfolio will have a higher standard deviation of returns, which means that it will be more volatile. Third, the portfolio will not be efficient, which means that it will not offer the highest expected return for a specific level of risk.")
+weights, fig = show_efficient_froniter(mu, Sigma, long_only=True)
+st.pyplot(fig)
+st.table(ordered_dict_to_dataframe(weights))
+
+st.subheader("Efficient Frontier (Long/ Short)")
+st.caption("""When it comes to efficient frontier analysis in a long-only sense, having few assets that are highly correlated doesn't make sense because it doesn't provide enough diversification to reduce risk. However, in a long-short manner, having few assets that are highly correlated can work because it allows for a market-neutral strategy that can profit from the difference in performance between the two assets.""")
+weights, fig = show_efficient_froniter(mu, Sigma, long_only=False)
+st.pyplot(fig)
+st.table(ordered_dict_to_dataframe(weights))
+
+st.header("Maximum Sharpe Ratio Portfolio")
+st.caption("The maximum sharpe ratio portfolio is a portfolio that has been optimized to provide the highest Sharpe Ratio, which is a metric that compares the amount of return versus the amount of risk, based on historical data. The Sharpe Ratio is calculated by dividing the excess return of a portfolio by its volatility, which is a measure of risk. The Maximum Sharpe Ratio Portfolio is well-suited for risk-averse investors with moderate growth expectations. The Sharpe Ratio is widely used among investors to evaluate investment performance, and a higher Sharpe Ratio indicates a better risk-adjusted return. Generally, a Sharpe Ratio between 1 and 2 is considered good, a ratio between 2 and 3 is very good, and any result higher than 3 is excellent")
+ef = EfficientFrontier(mu, Sigma, weight_bounds=(0, 1))
+sharpe_weights = ef.max_sharpe()
+ret, vol, sharpe = ef.portfolio_performance()
+st.write(f"Expected annual return: {round(ret*100, 0)}%")
+st.write(f"Annual volatility: {round(vol*100, 0)}%")
+st.write(f"Sharpe Ratio: {round(sharpe, 2)}")
+st.table(ordered_dict_to_dataframe(sharpe_weights))
+
+
+st.header("Minimum Variance Portfolio")
+st.caption("A minimum variance portfolio is a collection of securities that are combined to minimize the price volatility of the overall portfolio. It is an investing strategy that uses diversification to minimize risk and maximize profits. A minimum variance portfolio holds individual, volatile securities that aren't correlated with one another. For each level of return, the portfolio with the minimum risk will be selected by a risk-averse investor, creating a minimum-variance frontier - a collection of all the minimum-variance (minimum-standard deviation) portfolios. The portion of the minimum-variance curve that lies above and to the right of the global minimum variance portfolio is known as the Markowitz efficient frontier.")
+ef = EfficientFrontier(mu, Sigma, weight_bounds=(0, 1))
+min_vol_weights = ef.min_volatility()
+ret, vol, sharpe = ef.portfolio_performance()
+st.write(f"Expected annual return: {round(ret*100, 0)}%")
+st.write(f"Annual volatility: {round(vol*100, 0)}%")
+st.write(f"Sharpe Ratio: {round(sharpe, 2)}")
+st.table(ordered_dict_to_dataframe(min_vol_weights))
--- utils.py
+++ utils.py
@@ -9,8 +9,56 @@ from pypfopt.efficient_frontier import EfficientFrontier
 from Historic_Crypto import Cryptocurrencies, HistoricalData, LiveCryptoData
 import numpy as np
 from datetime import datetime
+import streamlit as st
 
-def show_efficient_froniter(long_only=True):
+@st.cache_data
+def download_valid_crypto_list():
+  crypto_pairs = Cryptocurrencies().find_crypto_pairs()
+  valid_crypto_pairs = crypto_pairs[(crypto_pairs["fx_stablecoin"]==False)&(crypto_pairs["status"]=="online")&(crypto_pairs["display_name"].str.endswith("/USD"))]
+  return valid_crypto_pairs["id"].unique().tolist()
+
+def ordered_dict_to_dataframe(ordered_dict):
+    df = pd.DataFrame(list(ordered_dict.items()), columns=['TICKER', 'WEIGHT'])
+    df = df.set_index("TICKER")
+    return df
+
+def plot_covariance(cov_matrix, plot_correlation=False, show_tickers=True, **kwargs):
+    """
+    Generate a basic plot of the covariance (or correlation) matrix, given a
+    covariance matrix.
+
+    :param cov_matrix: covariance matrix
+    :type cov_matrix: pd.DataFrame or np.ndarray
+    :param plot_correlation: whether to plot the correlation matrix instead, defaults to False.
+    :type plot_correlation: bool, optional
+    :param show_tickers: whether to use tickers as labels (not recommended for large portfolios),
+                        defaults to True
+    :type show_tickers: bool, optional
+
+    :return: matplotlib axis
+    :rtype: matplotlib.axes object
+    """
+    if plot_correlation:
+        matrix = risk_models.cov_to_corr(cov_matrix)
+    else:
+        matrix = cov_matrix
+    fig, ax = plt.subplots()
+
+    cax = ax.imshow(matrix)
+    fig.colorbar(cax)
+
+    if show_tickers:
+        ax.set_xticks(np.arange(0, matrix.shape[0], 1))
+        ax.set_xticklabels(matrix.index)
+        ax.set_yticks(np.arange(0, matrix.shape[0], 1))
+        ax.set_yticklabels(matrix.index)
+        plt.xticks(rotation=90)
+
+    plotting._plot_io(**kwargs)
+
+    return fig
+
+def show_efficient_froniter(mu, Sigma, long_only=True, **kwargs):
   ef = EfficientFrontier(mu, Sigma, weight_bounds=(0, 1) if long_only else (None, None))
   fig, ax = plt.subplots()
   ef_max_sharpe = ef.deepcopy()
@@ -32,11 +80,10 @@ def show_efficient_froniter(long_only=True):
   # Output
   ax.set_title("Efficient Frontier with random portfolios")
   ax.legend()
-  plt.tight_layout()
-  plt.savefig("ef_scatter.png", dpi=200)
-  plt.show()
-  return ef.clean_weights()
+  plotting._plot_io(**kwargs)
+  return ef.clean_weights(), fig
 
+@st.cache_data
 def download_crypto_data(selected_pairs, start_date, interval = 86400):
   temp_dict = {}
   for pair in selected_pairs:
